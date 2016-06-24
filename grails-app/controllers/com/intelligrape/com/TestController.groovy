72a95fcd441e91ef8f33fbc3bea9b99abdfd9bb3
Ashu <ashuashu20691@gmail.com>
2016-06-24 20:14:49 +0530

    my first commit on heroku

---
+++ grails-app/controllers/com/intelligrape/com/TestController.groovy
@@ -0,0 +1,9 @@
+package com.intelligrape.com
+
+class TestController {
+
+    def index() {
+        println("Bhaiyaaaaaaa main....................hit hua...  :P")
+        render(view: "index")
+    }
+}
