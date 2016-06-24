3119bbc611782935e90c4bcffafbd262932f6aaf
Ashu <ashuashu20691@gmail.com>
2016-06-24 20:48:15 +0530

    Added database config

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -34,9 +34,9 @@ environments {
             driverClassName = "org.postgresql.Driver"
             dialect = org.hibernate.dialect.PostgreSQLDialect
             uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
-            url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path
-            username = uri.userInfo.split(":")[0]
-            password = uri.userInfo.split(":")[1]
+            url = "postgres://ixubxkrphywzna:kZKa1Lv0XQxteIM7RG2DVXYKNr@ec2-54-235-102-235.compute-1.amazonaws.com:5432/d9lsk9adtd1kje"
+            username = "ixubxkrphywzna"
+            password = "kZKa1Lv0XQxteIM7RG2DVXYKNr"
         }
     }
 }
